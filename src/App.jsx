8c13047e10e9d1490b84e9382b03c7e20550c97f
Viktor Svoboda <viksvoboda@proton.me>
2024-05-14 18:44:31 +0200

    error msg for >7 days period

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,46 @@ import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   const [asteroids, setAsteroids] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [selectedEndDate, setSelectedEndDate] = useState(null);
+  // const [selectedDate, setSelectedDate] = useState("2024-05-13");
+  // const [selectedEndDate, setSelectedEndDate] = useState("2024-05-20");
+  const apiKey = "3lIka5CXiRJQ0hHNEPilFSSFkP8tg33KRaDeQyvM";
+
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+    checkInterval(date, endDate);
+  };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date);
+    checkInterval(startDate, date);
+  };
+
+  const checkInterval = (start, end) => {
+    if (start && end) {
+      const intervalInDays = Math.abs((end - start) / (1000 * 60 * 60 * 24));
+      if (intervalInDays > 7) {
+        setErrorMessage(
+          "Interval between dates must be less than or equal to 7 days."
+        );
+      } else {
+        setErrorMessage("");
+      }
+    }
+  };
 
   // function getClosebyAsteroids() {
   //   axios
   //     .get(
-  //       "https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-05-13&end_date=2024-05-20&api_key=3lIka5CXiRJQ0hHNEPilFSSFkP8tg33KRaDeQyvM"
+  //       `https://api.nasa.gov/neo/rest/v1/feed?start_date=${selectedDate}&end_date=${selectedEndDate}&api_key=${apiKey}`
   //     )
   //     .then((response) => {
-  //       console.log(response.data.near_earth_objects["2024-05-13"]);
-  //       setAsteroids(response.data.near_earth_objects["2024-05-13"]);
+  //       console.log(response.data.near_earth_objects[0]);
+  //       // adjust to select all
+  //       setAsteroids(response.data.near_earth_objects[0]);
   //     })
   //     .catch((error) => {
   //       console.error("Error fetching data:", error);
@@ -27,14 +56,14 @@ function App() {
       <div style={{ display: "flex", flexFlow: "column" }}>
         <h2>Select Start Date</h2>
         <ReactDatePicker
-          selected={selectedDate}
-          onChange={(date) => setSelectedDate(date)}
+          selected={startDate}
+          onChange={(date) => handleStartDateChange(date)}
           dateFormat="yyyy-MM-dd"
         />
         <h2>Select End Date</h2>
         <ReactDatePicker
-          selected={selectedEndDate}
-          onChange={(date) => setSelectedEndDate(date)}
+          selected={endDate}
+          onChange={(date) => handleEndDateChange(date)}
           dateFormat="yyyy-MM-dd"
         />
         <button
@@ -45,6 +74,7 @@ function App() {
         >
           submit
         </button>
+        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       </div>
       <div>
         {asteroids.map((asteroid) => {
