b8da195f6ca2f8bd9f3367bd05e702d88e9a4826
Viktor Svoboda <viksvoboda@proton.me>
2024-05-19 19:44:27 +0200

    improved error handling on backend

--- server.js
+++ server.js
@@ -15,14 +15,29 @@ app.get("/api/neo", async (req, res) => {
     const apiKey = "3lIka5CXiRJQ0hHNEPilFSSFkP8tg33KRaDeQyvM";
     const startDate = req.query.start_date;
     const endDate = req.query.end_date;
+
     const apiUrl = `${url}?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`;
     const response = await fetch(apiUrl);
-    const data = await response.json();
 
+    // Check if the response status is not ok (200-299) and throw an error if not
+    if (!response.ok) {
+      const errorDetails = await response.json();
+      const error = new Error(
+        `Failed to fetch data from NASA API: ${response.statusText}`
+      );
+      error.status = response.status;
+      error.details = errorDetails;
+      throw error;
+    }
+
+    const data = await response.json();
     res.json(data);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error });
+    console.error("Error fetching data from NASA API:", error);
+    res.status(error.status || 500).json({
+      error: error.message || "Internal server error",
+      details: error.details || error.toString(),
+    });
   }
 });
 
